d70b260a87127b97ceb2cdcbbe35c08cb4b90022
Taren Pang <tarenpang@gmail.com>
2023-03-14 11:46:15 -0700

    initial commit

---
+++ config.js
@@ -0,0 +1,9 @@
+const GENESIS_DATA = {
+	// screen case syntax == hard coded global values
+	timestamp: 1,
+	lastHash: '-----',
+	hash: 'hash-one',
+	data: [],
+};
+
+module.exports = { GENESIS_DATA };
