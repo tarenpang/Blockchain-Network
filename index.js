a150187e11bd48725d99dff4fbdc038365125d1f
Taren Pang <tarenpang@gmail.com>
2023-03-29 11:06:45 -0700

    added GET Transaction Pool Map endpoint

--- index.js
+++ index.js
@@ -53,6 +53,10 @@ app.post('/api/transact', (req, res) => {
 	res.json({ type: 'success', transaction });
 });
 
+app.get('/api/transaction-pool-map', (req, res) => {
+	res.json(transactionPool.transactionMap);
+});
+
 const syncChains = () => {
 	request(
 		{ url: `${ROOT_NODE_ADDRESS}/api/blocks` },
