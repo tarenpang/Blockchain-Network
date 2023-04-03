cd16a005bfb04e1a61260836191e5fd845a1777a
Taren Pang <tarenpang@gmail.com>
2023-04-03 15:54:54 -0700

    added Visualize the Blocks

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,49 +1,52 @@
-import React, { useState, useEffect } from 'react';
+// import React, { useState, useEffect } from 'react';
 
-function App() {
-	const [walletInfo, setWalletInfo] = useState({});
+// function App() {
+// 	const [walletInfo, setWalletInfo] = useState({});
 
-	useEffect(() => {
-		fetch('http://localhost:3000/api/wallet-info')
-			.then((response) => response.json())
-			.then((json) => setWalletInfo(json));
-	}, []);
-
-	const { address, balance } = walletInfo;
-
-	return (
-		<div>
-			<div>Welcome to the blockchain...</div>
-			<div>Address: {address}</div>
-			<div>Balance: {balance}</div>
-		</div>
-	);
-}
-
-export default App;
-
-// import React, { Component } from 'react';
-
-// class App extends Component {
-// 	state = { walletInfo: {} };
-
-// 	componentDidMount() {
+// 	useEffect(() => {
 // 		fetch('http://localhost:3000/api/wallet-info')
 // 			.then((response) => response.json())
-// 			.then((json) => this.setState({ walletInfo: json }));
-// 	}
+// 			.then((json) => setWalletInfo(json));
+// 	}, []);
 
-// 	render() {
-// 		const { address, balance } = this.state.walletInfo;
+// 	const { address, balance } = walletInfo;
 
-// 		return (
-// 			<div>
-// 				<div>Welcome to the blockchain...</div>
-// 				<div>Address: {address}</div>
-// 				<div>Balance: {balance}</div>
-// 			</div>
-// 		);
-// 	}
+// 	return (
+// 		<div>
+// 			<div>Welcome to the blockchain...</div>
+// 			<div>Address: {address}</div>
+// 			<div>Balance: {balance}</div>
+// 		</div>
+// 	);
 // }
 
 // export default App;
+
+import React, { Component } from 'react';
+import Blocks from './Blocks';
+
+class App extends Component {
+	state = { walletInfo: {} };
+
+	componentDidMount() {
+		fetch('http://localhost:3000/api/wallet-info')
+			.then((response) => response.json())
+			.then((json) => this.setState({ walletInfo: json }));
+	}
+
+	render() {
+		const { address, balance } = this.state.walletInfo;
+
+		return (
+			<div>
+				<div>Welcome to the blockchain...</div>
+				<div>Address: {address}</div>
+				<div>Balance: {balance}</div>
+				<br />
+				<Blocks />
+			</div>
+		);
+	}
+}
+
+export default App;
