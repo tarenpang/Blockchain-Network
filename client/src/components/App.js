a0e3e20ec203d7f049461228b8bc404042cce0b3
Taren Pang <tarenpang@gmail.com>
2023-04-03 15:09:57 -0700

    fetched Display Wallet-info

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,7 +1,23 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 function App() {
-	return <div>Welcome to the blockchain...</div>;
+	const [walletInfo, setWalletInfo] = useState({});
+
+	useEffect(() => {
+		fetch('http://localhost:3000/api/wallet-info')
+			.then((response) => response.json())
+			.then((json) => setWalletInfo(json));
+	}, []);
+
+	const { address, balance } = walletInfo;
+
+	return (
+		<div>
+			<div>Welcome to the blockchain...</div>
+			<div>Address: {address}</div>
+			<div>Balance: {balance}</div>
+		</div>
+	);
 }
 
 export default App;
@@ -9,13 +25,25 @@ export default App;
 // import React, { Component } from 'react';
 
 // class App extends Component {
-//   render() {
-//     return (
-//       <div>
-//         Welcome to the blockchain...
-//       </div>
-//     );
-//   }
+// 	state = { walletInfo: {} };
+
+// 	componentDidMount() {
+// 		fetch('http://localhost:3000/api/wallet-info')
+// 			.then((response) => response.json())
+// 			.then((json) => this.setState({ walletInfo: json }));
+// 	}
+
+// 	render() {
+// 		const { address, balance } = this.state.walletInfo;
+
+// 		return (
+// 			<div>
+// 				<div>Welcome to the blockchain...</div>
+// 				<div>Address: {address}</div>
+// 				<div>Balance: {balance}</div>
+// 			</div>
+// 		);
+// 	}
 // }
 
 // export default App;
