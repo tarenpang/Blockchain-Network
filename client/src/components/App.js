90ca2ef7121548e63a376d8dca561a7d159bacc3
Taren Pang <tarenpang@gmail.com>
2023-04-03 14:30:19 -0700

    added React App Component

---
+++ client/src/components/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+function App() {
+	return <div>Welcome to the blockchain...</div>;
+}
+
+export default App;
+
+// import React, { Component } from 'react';
+
+// class App extends Component {
+//   render() {
+//     return (
+//       <div>
+//         Welcome to the blockchain...
+//       </div>
+//     );
+//   }
+// }
+
+// export default App;
