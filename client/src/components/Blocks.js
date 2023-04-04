aeaab4ae416a24936837736216225a676e20ef1f
Taren Pang <tarenpang@gmail.com>
2023-04-03 17:09:03 -0700

    added the Block Component with Props

--- client/src/components/Blocks.js
+++ client/src/components/Blocks.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Block from './Block';
 
 class Blocks extends Component {
 	state = { blocks: [] };
@@ -16,11 +17,7 @@ class Blocks extends Component {
 			<div>
 				<h3>Blocks</h3>
 				{this.state.blocks.map((block) => {
-					return (
-						<div key={block.hash} className="Block">
-							{block.hash}
-						</div>
-					);
+					return <Block key={block.hash} block={block} />;
 				})}
 			</div>
 		);
