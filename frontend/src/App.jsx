414bad618f11e3726546fc122db770102a2097d2
Taren Pang <tarenpang@gmail.com>
2023-04-13 18:57:54 -0700

    various frontend updates

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// import { useState } from "react"
+// import "bootstrap/dist/css/bootstrap.min.css"
+import Navbar from "./navigation/Navbar"
+// import "./App.css"
+import "../custom.css"
+import logo from "./assets/logo-04.png"
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+	return (
+		<div>
+			<Navbar />
+			<br />
+			<img className="logo" src={logo}></img>
+			<h1>Hello Blockchain App</h1>
+		</div>
+	)
+	// <h1 className="text-3xl font-bold underline">Hello world!</h1>
 }
 
 export default App
