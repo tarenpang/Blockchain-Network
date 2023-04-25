71ef4df18e57800f64469261b01a8e4318e7f78b
Josh Baum <joshbaum101@gmail.com>
2023-04-24 21:00:38 -0400

    Beginning routes

---
+++ frontend/src/pages/Wallet.jsx
@@ -0,0 +1,14 @@
+import "../../custom.css";
+import React from "react";
+
+function Wallet() {
+    return (
+      <div>
+        <br />
+        <h1>Wallet</h1>
+      </div>
+    );
+   
+  }
+  
+  export default Wallet;
