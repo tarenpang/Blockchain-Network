788755e8545e5b8c67bd76cb89a68d1b78ab9a43
Josh Baum <joshbaum101@gmail.com>
2023-05-08 18:09:11 -0400

    wallet landing

--- frontend/src/pages/Wallet.jsx
+++ frontend/src/pages/Wallet.jsx
@@ -5,10 +5,64 @@ import { useState, useEffect } from "react";
 import { Button, Form, InputGroup } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import secureLocalStorage from "react-secure-storage";
+import "../../../backend/utils/cryptoUtils";
+import { pubKeyToAddress } from "../../../backend/utils/cryptoUtils";
+
+// var EC = require('elliptic').ec;
+// var secp256k1 = new EC('secp256k1');
+// const CryptoJS = require("crypto-js");
+
+const generateWallet = (keyPair) => {
+	const privateKey = keyPair.getPrivate().toString(16);
+	const publicKey =
+	  keyPair.getPublic().getX().toString(16) +
+	  (keyPair.getPublic().getY().isOdd() ? "1" : "0");
+	// const address = CryptoJS.RIPEMD160(publicKey).toString();
+	const address = pubKeyToAddress(publicKey);
+  
+	return {
+	  privateKey,
+	  publicKey,
+	  address
+	}
+  }
 
 function Wallet() {
 	const [allConfirmedTransactions, setAllConfirmedTransactions] = useState([]);
 	const [loggedIn, setLoggedIn] = useState(secureLocalStorage.getItem("loggedIn"));
+	const [ generatedKeys, setGeneratedKeys ] = useState("");
+
+	const handleLogin = () => {
+		if(!loggedIn){
+			secureLocalStorage.setItem("loggedIn", true);
+			setLoggedIn(true);
+		}else if (loggedIn){
+			setLoggedIn(false);
+			secureLocalStorage.clear();
+		}
+	}
+
+	const handleGeneration = () => {
+		let keyPair = secp256k1.genKeyPair();
+		const wallet = generateWallet(keyPair);
+		console.log("New wallet:" + wallet);
+
+		secureLocalStorage.setItem("privKey", wallet.privateKey);
+		secureLocalStorage.setItem("pubKey", wallet.publicKey);
+		secureLocalStorage.setItem("address", wallet.address);
+
+		setGeneratedKeys("PRIVATE KEY: " +
+		wallet.privateKey +
+		"\n" +
+		"\n" +
+		"PUBLIC KEY: " +
+		wallet.publicKey +
+		"\n" +
+		"\n" +
+		"BLOCKCHAIN ADDRESS: " + 
+		wallet.address
+		);
+	}
 
 	// useEffect(() => {
 	// 	(async function loadData() {
@@ -28,6 +82,7 @@ function Wallet() {
 	// };
 
 	return (
+	
 		<div>
 			<br />
 			<h1>Welcome to the IndiGold Wallet</h1>
@@ -41,7 +96,7 @@ function Wallet() {
 						<br></br>
 						<p>Click the button below to create a new wallet. Each wallet is generated with a new address used to send and store your IndiGold. You can recover your wallet at anytime with the generated privateKey. Don't forget to save your wallet credentials!  </p>
 						<br></br>
-						<Button type="submit">Search</Button>
+						<Button type="submit">Create</Button>
 					</div>
 				</div>
 
